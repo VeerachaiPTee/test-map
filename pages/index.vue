5f5ef43c24dcfaba0ae729a8e96a8c7d81f60b9b
VeerachaiPTee <devtee.pp@gmail.com>
2023-11-17 12:27:07 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- กำหนดพื้นที่ของแผนที่บนเว็บ  -->
+  <div style="height: 100dvh" id="map"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      zoom: 6,
+      center: [14.870032, 102.992541],
+      map: null,
+    }
+  },
+  methods: {
+    initmap() {
+      const map = L.map('map').setView(this.center, this.zoom)
+      this.map = map
+
+      L.tileLayer(
+        'https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=ODQYjrfwMX2CleHUV2jQ'
+      ).addTo(map)
+    },
+  },
+  mounted() {
+    this.initmap()
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ plugins/plugins.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+Vue.prototype.$L = L 
